939d94d23f8065eab7fa68ccedb133aec5911e30
Mat <Keons@LAPTOP-618U4BIL>
2019-08-12 21:22:12 -0600

    File Chooser y Buffer Reader

--- NotasCSV/src/notascsv/NotasCSV.java
+++ NotasCSV/src/notascsv/NotasCSV.java
@@ -18,8 +18,103 @@ import javafx.stage.Stage;
  *
  * @author Mathiw Rojas
  */
+
 public class NotasCSV extends Application {
-   
+    class Estudiante{
+        private String nombre;
+        private String correo;
+        private int tel;
+        private String nickname;
+        private char tipoEstudiante;
+        private int promExamenes;
+        private int promQuices;
+        private int promTareas;
+        private int proyecto1;
+        private int proyecto2;
+        private int proyecto3;
+        public Estudiante(String newNombre, String newCorreo, int newTel, String newNickname, char newTipo, int newPromE, int newPromQ, int newPromT, int newProyecto1, int newProyecto2, int newProyecto3){
+            nombre = newNombre;
+            correo = newCorreo;
+            tel = newTel;
+            nickname = newNickname;
+            tipoEstudiante = newTipo;
+            promExamenes = newPromE;
+            promQuices = newPromQ;
+            promTareas = newPromT;
+            proyecto1 = newProyecto1;
+            proyecto2 = newProyecto2;
+            proyecto3 = newProyecto3;
+        }
+        
+        public String getNombre(){
+            return nombre;
+        }
+        public String getCorreo(){
+            return correo;
+        }
+        public int getTel(){
+            return tel;
+        }
+        public String getNickname(){
+            return nickname;
+        }
+        public char getTipoEstudiante(){
+            return tipoEstudiante;
+        }
+        public int getPromExamenes(){
+            return promExamenes;
+        }
+        public int getPromQuices(){
+            return promQuices;
+        }
+        public int getPromTareas(){
+            return promTareas;
+        }
+        public int getProyecto1(){
+            return proyecto1;
+        }
+        public int getProyecto2(){
+            return proyecto2;            
+        }
+        public int getProyecto3(){
+            return proyecto3;            
+        }
+    
+    }
+    
+    
+    
+    class EstudianteTipoA{
+        private int notaPromedioEQT;
+        Estudiante estudiante;
+        public EstudianteTipoA(String newNombre, String newCorreo, int newTel, String newNickname, int newPromE, int newPromQ, int newPromT, int newProyecto1, int newProyecto2, int newProyecto3){
+            estudiante = new Estudiante(newNombre, newCorreo, newTel, newNickname, 'A', newPromE, newPromQ, newPromT, newProyecto1, newProyecto2, newProyecto3);
+            notaPromedioEQT = (newPromE + newPromQ + newPromT)/3;
+        }
+        
+        public int getNotaPromedioEQT(){
+            return notaPromedioEQT;
+        }
+        
+    }
+    
+    
+    
+    class EstudianteTipoB{
+        private int notaPromedioProyectos;
+        Estudiante estudiante;
+        public EstudianteTipoB(String newNombre, String newCorreo, int newTel, String newNickname, int newPromE, int newPromQ, int newPromT, int newProyecto1, int newProyecto2, int newProyecto3){
+            estudiante = new Estudiante(newNombre, newCorreo, newTel, newNickname, 'B', newPromE, newPromQ, newPromT, newProyecto1, newProyecto2, newProyecto3);
+            notaPromedioProyectos = (newProyecto1 + newProyecto2 + newProyecto3)/3;
+        }
+        
+        public int getNotaPromedioProyectos(){
+            return notaPromedioProyectos;
+        }
+        
+        
+    }
+    
     @Override
     public void start(Stage primaryStage) {        
         primaryStage.setTitle("Notas Estudiantes");
