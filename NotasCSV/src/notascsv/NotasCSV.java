db40fd74616c20c26de80cbd4d95f19a79ddd674
Mat <>
2019-08-15 23:59:09 -0600

    Calculos de nota final, aplicacion de sobrecarga y documentacion

--- NotasCSV/src/notascsv/NotasCSV.java
+++ NotasCSV/src/notascsv/NotasCSV.java
@@ -28,21 +28,44 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 public class NotasCSV extends Application {
     
+    /**Decalracion de la clase Estudiante
+     *@author Mathiw Rojas
+     * @version 15/8/19
+     * Aqui se utiliza los conceptos de clase, encapsulamiento, atributos
+     */
     public class Estudiante{
+        //declaracion de los atributos privados
         private String carnet;
         private String nombre;
         private String correo;
         private String tel;
         private String nickname;
-        private char tipoEstudiante;
-        private int promExamenes;
-        private int promQuices;
-        private int promTareas;
+        private String tipoEstudiante;
+        private float promExamenes;
+        private float promQuices;
+        private float promTareas;
         private int proyecto1;
         private int proyecto2;
         private int proyecto3;
         
-        public Estudiante(String newCarnet, String newNombre, String newCorreo, String newTel, String newNickname, char newTipo, int newPromE, int newPromQ, int newPromT, int newProyecto1, int newProyecto2, int newProyecto3){
+        
+        /** creacion del constructor
+         * @param newCarnet 
+         * @param newNombre
+         * @param newCorreo
+         * @param newTel
+         * @param newNickname
+         * @param newTipo
+         * Los parametros antes de este comentario son datos personales del estudiante
+         * @param newPromE
+         * @param newPromQ
+         * @param newPromT
+         * @param newProyecto1
+         * @param newProyecto2
+         * @param newProyecto3
+         * Los parametros antes de este comentario son notas del estudiante
+         */
+        public Estudiante(String newCarnet, String newNombre, String newCorreo, String newTel, String newNickname, String newTipo, int newPromE, int newPromQ, int newPromT, int newProyecto1, int newProyecto2, int newProyecto3){
             carnet = newCarnet;
             nombre = newNombre;
             correo = newCorreo;
@@ -57,64 +80,106 @@ public class NotasCSV extends Application {
             proyecto3 = newProyecto3;
         }
         
+        /**@return Retorna el carnet del estudiante  */
         public String getCarnet(){
             return carnet;
         }
         
+        /**@return Retorna el nombre del estudiante  */
         public String getNombre(){
             return nombre;
         }
         
+        /**@return Retorna el correo del estudiante  */
         public String getCorreo(){
             return correo;
         }
         
+        /**@return Retorna el Telefono del estudiante  */
         public String getTel(){
             return tel;
         }
         
+        /**@return Retorna el NickName del estudiante  */
         public String getNickname(){
             return nickname;
         }
         
-        public char getTipoEstudiante(){
+        /**@return Retorna el tipo de estudiante al cual pertenece el estudiante  */
+        public String getTipoEstudiante(){
             return tipoEstudiante;
         }
         
-        public int getPromExamenes(){
+        /**@return Retorna el promedio de los examenes del estudiante  */
+        public float getPromExamenes(){
             return promExamenes;
         }
         
-        public int getPromQuices(){
+        /**@return Retorna el promedio de los quices del estudiante  */
+        public float getPromQuices(){
             return promQuices;
         }
         
-        public int getPromTareas(){
+        /**@return Retorna el promedio de las tareas del estudiante  */
+        public float getPromTareas(){
             return promTareas;
         }
         
+        /**@return Retorna la nota del primer proyecto del estudiante  */
         public int getProyecto1(){
             return proyecto1;
         }
         
+        /**@return Retorna la nota del segundo proyecto del estudiante  */
         public int getProyecto2(){
             return proyecto2;            
         }
        
+        /**@return Retorna la nota del tercer proyecto del estudiante  */
         public int getProyecto3(){
             return proyecto3;            
+        }
+        
+        /**Metodo que calcula el promedio de 3 notas
+         * @param nota1,@param nota2, @param nota 3 son parametros de notas obtenidas por el estudiante 
+         * @return Retorna el promedio de las 3 notas recibidas
+         Aqui se utiliza el concepto de sobrecarga*/
+        public float CalculaPromedio(float nota1, float nota2, float nota3){
+            return (nota1 + nota2 + nota3)/3;
+        }
+        
+        /**Metodo que calcula la nota final del estudiante dependiendo de que tipo sea
+         *@param prom es la nota del promedio que se le calculo a ese tipo de estudiante
+         * @param tipo es el tipo de estudiante al que el estudiante pertenece
+         * @return Retorna la nota final del estudiante
+         * Aqui se utiliza el concepto de sobrecarga
+         */
+        public float CalculaPromedio(float prom, String tipo){
+            if(tipo.equals("A")){
+                return (float) (prom * 0.65 + ((proyecto1 + proyecto2 + proyecto3)/3) * 0.35);
+            }else{
+                return (float) (prom * 0.35 + ((promTareas + promQuices + promExamenes)/3) * 0.65);
+            }
         }
     
     }
     
     
-    
+    /**Decalracion de la clase EstudianteTipoA
+     *@author Mathiw Rojas
+     * @version 15/8/19 
+     */
     public class EstudianteTipoA{
-        private int notaPromedioEQT;
+        
+        //declaracion de los atributos privados
+        private float notaPromedioEQT;
         private Estudiante estudiante;
+        private float notaFinal;
+        
         public EstudianteTipoA(String newCarnet,String newNombre, String newCorreo, String newTel, String newNickname, int newPromE, int newPromQ, int newPromT, int newProyecto1, int newProyecto2, int newProyecto3){
-            estudiante = new Estudiante(newCarnet,newNombre, newCorreo, newTel, newNickname, 'A', newPromE, newPromQ, newPromT, newProyecto1, newProyecto2, newProyecto3);
-            this.notaPromedioEQT = (newPromE + newPromQ + newPromT)/3;
+            estudiante = new Estudiante(newCarnet,newNombre, newCorreo, newTel, newNickname, "A", newPromE, newPromQ, newPromT, newProyecto1, newProyecto2, newProyecto3);
+            notaPromedioEQT= estudiante.CalculaPromedio(newPromT, newPromE, newPromQ);
+            notaFinal = estudiante.CalculaPromedio(notaPromedioEQT, "A");
         }
         
         public String getCarnet(){
@@ -137,19 +202,19 @@ public class NotasCSV extends Application {
             return estudiante.nickname;
         }
         
-        public char getTipoEstudiante(){
+        public String getTipoEstudiante(){
             return estudiante.tipoEstudiante;
         }
         
-        public int getPromExamenes(){
+        public float getPromExamenes(){
             return estudiante.promExamenes;
         }
         
-        public int getPromQuices(){
+        public float getPromQuices(){
             return estudiante.promQuices;
         }
         
-        public int getPromTareas(){
+        public float getPromTareas(){
             return estudiante.promTareas;
         }
         
@@ -165,8 +230,11 @@ public class NotasCSV extends Application {
             return estudiante.proyecto3;            
         }
     
-        public int getNotaPromedioEQT(){
+        public float getNotaPromedioEQT(){
             return notaPromedioEQT;
+        }
+        public float getNotaFinal(){
+            return notaFinal;
         }
         
     }
@@ -175,12 +243,14 @@ public class NotasCSV extends Application {
     
     public class EstudianteTipoB{
         
-        private int notaPromedioProyectos;
+        private float notaPromedioProyectos;
+        private float notaFinal;
         Estudiante estudiante;
         
         public EstudianteTipoB(String newCarnet, String newNombre, String newCorreo, String newTel, String newNickname, int newPromE, int newPromQ, int newPromT, int newProyecto1, int newProyecto2, int newProyecto3){
-            estudiante = new Estudiante(newCarnet, newNombre, newCorreo, newTel, newNickname, 'B', newPromE, newPromQ, newPromT, newProyecto1, newProyecto2, newProyecto3);
-            this.notaPromedioProyectos = (newProyecto1 + newProyecto2 + newProyecto3)/3;
+            estudiante = new Estudiante(newCarnet, newNombre, newCorreo, newTel, newNickname, "B", newPromE, newPromQ, newPromT, newProyecto1, newProyecto2, newProyecto3);
+            notaPromedioProyectos = estudiante.CalculaPromedio(newProyecto1, newProyecto2, newProyecto3);
+            notaFinal = estudiante.CalculaPromedio(notaPromedioProyectos, "B");
         }
         
         
@@ -204,19 +274,19 @@ public class NotasCSV extends Application {
             return estudiante.nickname;
         }
         
-        public char getTipoEstudiante(){
+        public String getTipoEstudiante(){
             return estudiante.tipoEstudiante;
         }
         
-        public int getPromExamenes(){
+        public float getPromExamenes(){
             return estudiante.promExamenes;
         }
         
-        public int getPromQuices(){
+        public float getPromQuices(){
             return estudiante.promQuices;
         }
         
-        public int getPromTareas(){
+        public float getPromTareas(){
             return estudiante.promTareas;
         }
         
@@ -232,8 +302,12 @@ public class NotasCSV extends Application {
             return estudiante.proyecto3;            
         }
         
-        public int getNotaPromedioProyectos(){
+        public float getNotaPromedioProyectos(){
             return notaPromedioProyectos;
+        }
+        
+        public float getNotaFinal(){
+            return notaFinal;
         }
         
         
@@ -300,7 +374,7 @@ public class NotasCSV extends Application {
     
     public void Agnadir_Field(String[] field, Boolean isHeader){
         if(isHeader){
-            TableColumn col,colEQT,colProyectos;
+            TableColumn col,colEQT,colProyectos,colFinal;
             String[] columnValues = new String[]{"carnet","nombre","correo","tel","nickname","tipoEstudiante","promExamenes","promQuices","promTareas","proyecto1","proyecto2","proyecto3"}; 
             
             for(int i=0; i < field.length ;i++){
@@ -317,7 +391,11 @@ public class NotasCSV extends Application {
             colEQT = new TableColumn("Promedio EQT");
             colEQT.setCellValueFactory(new PropertyValueFactory<>("notaPromedioEQT"));
             colEQT.prefWidthProperty().bind(tableEstudiantes.widthProperty().multiply(0.08));
-            tableEstudiantes.getColumns().addAll(colProyectos,colEQT);
+            
+            colFinal = new TableColumn("Nota Final");
+            colFinal.setCellValueFactory(new PropertyValueFactory<>("notaFinal"));
+            colFinal.prefWidthProperty().bind(tableEstudiantes.widthProperty().multiply(0.08));
+            tableEstudiantes.getColumns().addAll(colProyectos,colEQT,colFinal);
             
         }else{
             if("A".equals(field[5])){
